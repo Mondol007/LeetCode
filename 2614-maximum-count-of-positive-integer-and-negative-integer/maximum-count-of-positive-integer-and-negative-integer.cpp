0797b3dcc942738bb5ac71ffad8c00c2eee34100
Mondol Mridul Provakar <mondolruet@gmail.com>
2025-03-13 04:25:37 +0600

    Time: 0 ms (100.00%) | Memory: 21.4 MB (75.95%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,21 +1,59 @@
-class Solution {
+// class Solution {
+// public:
+//     int maximumCount(vector<int>& nums) {
+//         int n = nums.size();
+//         int pos = 0, neg = 0;
+
+//         for(int i=0; i<n; i++){
+//             if(nums[i] > 0){
+//                 pos++;
+//             }
+//             else if(nums[i] < 0){
+//                 neg++;
+//             }
+//             else{
+//                 continue;
+//             }
+//         }
+
+//         return max(pos, neg);
+//     }
+// };
+
+//Binary Search
+
+class Solution{
 public:
-    int maximumCount(vector<int>& nums) {
-        int n = nums.size();
-        int pos = 0, neg = 0;
+int maximumCount(vector<int>& nums){
+    int n = nums.size();
+    int left = 0;
+    int right = n-1;
 
-        for(int i=0; i<n; i++){
-            if(nums[i] > 0){
-                pos++;
-            }
-            else if(nums[i] < 0){
-                neg++;
-            }
-            else{
-                continue;
-            }
+    while(left<=right){
+        int mid = left + (right-left) /2;
+        if(nums[mid] > 0){
+            right = mid - 1;
         }
-
-        return max(pos, neg);
+        else{
+            left = mid + 1;
+        }
     }
+    int pos = n - left;
+
+    left = 0;
+    right = n-1;
+
+    while (left <= right) {
+    int mid = left + (right - left) / 2;
+    if (nums[mid] < 0) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  int neg = right + 1;
+
+  return max(pos, neg);
+
+}
 };
